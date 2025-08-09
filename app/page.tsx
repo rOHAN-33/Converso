fbce16cb0312ce6db113b70e7df9b91a3a9f1672
rohannegi484@gmail.com <rohannegi484@gmail.com>
2025-08-09 21:32:36 +0530

    completed the setup and added components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <h1>this is the saas project</h1>
+      <Button>this is the buton</Button>
+    </div>
   )
 }
 
