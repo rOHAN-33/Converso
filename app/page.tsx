0ee78c1f4b77f9e1675d38361424420aa7d2e808
rohannegi484@gmail.com <rohannegi484@gmail.com>
2025-08-10 22:47:00 +0530

    added some blocks in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,55 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import React from 'react'
+import { recentSessions } from '../constants'
 
 const Page = () => {
-  return (
-    <div>
-      <h1>this is the saas project</h1>
-      <Button>this is the buton</Button>
-    </div>
+  return(
+   <main>
+ <h1>Popular Companion</h1>
+      <section className='home-section'>
+<CompanionCard
+  id  = "123"
+  name="neura the brainy exlorer"
+  topic = "Neural Network of the Brain"
+  subject ="science"
+  duration= {43}
+  color = "#ffda6e"
+></CompanionCard>
+<CompanionCard
+  id  = "123"
+  name="neura the brainy exlorer"
+  topic = "Neural Network of the Brain"
+  subject ="science"
+  duration= {43}
+  color = "#ffda6e"
+></CompanionCard>
+<CompanionCard
+  id  = "123"
+  name="neura the brainy exlorer"
+  topic = "Neural Network of the Brain"
+  subject ="science"
+  duration= {43}
+  color = "#ffda6e"
+></CompanionCard>
+      </section>
+
+      <section className='home-section'>
+          <CompanionsList
+            title="Recently completed session"
+            companions = {recentSessions}
+            classNames  = "w-2/3 max-lg:w-full"
+          ></CompanionsList>
+          <CTA></CTA>
+      </section>
+      
+
+   </main>
+    
+     
+   
   )
 }
 
---
+++ src/components/CompanionsList.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { cn } from '@/lib/utils'
+
+interface CompanionsListProps{
+    title:string,
+    companions? : Companion[] ,
+    classNames?: string ,
+}
+const CompanionsList = ({title, companions, classNames}:CompanionsListProps) => {
+  return (
+    <article className={cn("companion-list", classNames)}>
+        <h2 className='font-bold text-3xl'>Recent Session</h2>
+        <Table>
+  <TableCaption>A list of your recent invoices.</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Invoice</TableHead>
+      <TableHead>Status</TableHead>
+      <TableHead>Method</TableHead>
+      <TableHead className="text-right">Amount</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    <TableRow>
+      <TableCell className="font-medium">INV001</TableCell>
+      <TableCell>Paid</TableCell>
+      <TableCell>Credit Card</TableCell>
+      <TableCell className="text-right">$250.00</TableCell>
+    </TableRow>
+  </TableBody>
+</Table>
+    </article>
+  )
+}
+
+export default CompanionsList
