e8465253d1b45bb7b434674465170d122892f448
rohannegi484@gmail.com <rohannegi484@gmail.com>
2025-08-13 01:19:37 +0530

    added the payment gateway

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,8 +1,11 @@
+import { PricingTable } from '@clerk/nextjs'
 import React from 'react'
 
 const subscription = () => {
   return (
-    <div>subscription</div>
+    <div>
+      <PricingTable></PricingTable>
+    </div>
   )
 }
 
